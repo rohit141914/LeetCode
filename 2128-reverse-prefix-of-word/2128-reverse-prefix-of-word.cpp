934ef9558cf7e4b51d2b98dc3662d94d36f25cd3
Rohit Nain <115409470+rohit141914@users.noreply.github.com>
2024-12-05 22:35:35 +0530

    Time: 0 ms (100%), Space: 8.4 MB (6.03%) - LeetHub

---
+++ 2128-reverse-prefix-of-word/2128-reverse-prefix-of-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        
+        int l=word.length();
+        for(int i=0;i<l;i++){
+            if(word[i]==ch){
+                reverse(word.begin(),word.begin()+i+1);
+                break;
+            }
+        }
+        return word;
+    }
+};
