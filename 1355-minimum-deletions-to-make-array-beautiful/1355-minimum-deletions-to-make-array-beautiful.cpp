06938a9fea08dab44eebca115002978b9d4e2ad9
Rohit Nain <115409470+rohit141914@users.noreply.github.com>
2025-01-11 18:13:15 +0530

    Time: 0 ms (100%), Space: 125.2 MB (26.92%) - LeetHub

--- 1355-minimum-deletions-to-make-array-beautiful/1355-minimum-deletions-to-make-array-beautiful.cpp
+++ 1355-minimum-deletions-to-make-array-beautiful/1355-minimum-deletions-to-make-array-beautiful.cpp
@@ -4,19 +4,16 @@ public:
      int l=nums.size();
      int d=0;
     pair<int,int>p;
-    //  int p=nums[0];
     int c=0;
     p={nums[0],0};
      for(int i=1;i<l;i++){
         if(nums[i]==p.first){
             if(p.second%2==0){
-                // p={nums[i],i-d};
                 c++;
                 d++;
                 nums[i]=-1;
             }
             else{
-            // cout<<i<<" ";
             p.second++;
             }
         }
