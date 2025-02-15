4ddd9157b9bf9f021ae70dfde6167664f67b6e49
Rohit Nain <115409470+rohit141914@users.noreply.github.com>
2025-02-16 00:21:53 +0530

    Time: 49 ms (52.64%), Space: 78.4 MB (48.41%) - LeetHub

---
+++ 2378-sender-with-largest-word-count/2378-sender-with-largest-word-count.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    struct m{
+        bool operator()(pair<string,int>&a,pair<string,int>&b){
+            if(a.second==b.second){
+                return a.first>b.first;
+            }
+           return a.second>b.second;
+        }
+    };
+    string largestWordCount(vector<string>& messages, vector<string>& senders) {
+        unordered_map<string,int>mp;
+        int l=messages.size();
+        for(int i=0;i<l;i++){
+            int c=0;
+            c=count(messages[i].begin(),messages[i].end(),' ')+1;
+            mp[senders[i]]+=c;
+        }      
+        priority_queue<pair<string,int>,vector<pair<string,int>>,m>p;
+        for(auto a:mp){
+            // cout<<a.first<<" "<<a.second<<" ";
+            p.push({a.first,a.second});
+            if(p.size()>1){
+                p.pop();
+            }
+            // cout<<p.top().first<<"-";
+        }
+        return p.top().first;
+    }
+};
