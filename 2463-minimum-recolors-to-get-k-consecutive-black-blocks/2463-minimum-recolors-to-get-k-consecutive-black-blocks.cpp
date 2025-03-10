e93fd3a22d479f196fcd6b1109b6d8ec522540e5
Rohit Nain <115409470+rohit141914@users.noreply.github.com>
2025-03-10 15:35:38 +0530

    Time: 0 ms (100%), Space: 8.3 MB (89.76%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int minimumRecolors(string blocks, int k) {
-        queue<char> blockQueue;
-        int numWhites = 0;
-
-        while (blockQueue.size() < k) {
-            char currentChar = blocks[blockQueue.size()];
-            if (currentChar == 'W') numWhites++;
-            blockQueue.push(currentChar);
+        int c=0;
+        int ans=0;
+        int l=blocks.size();
+        for(int i=0;i<k;i++){
+            if(blocks[i]=='W')c++;
         }
-
-        //  minimum
-        int numRecolors = numWhites;
-        for (int i = k; i < blocks.size(); i++) {
-            if (blockQueue.front() == 'W') numWhites--;
-            blockQueue.pop();
-
-            if (blocks[i] == 'W') numWhites++;
-            blockQueue.push(blocks[i]);
-
-            // Update minimum
-            numRecolors = min(numRecolors, numWhites);
+        ans=c;
+        for(int i=k;i<l;i++){
+            if(blocks[i]=='W')c++;
+            if(blocks[i-k]=='W')c--;
+            ans=min(c,ans);
         }
-        return numRecolors;
+        return ans;
     }
 };
