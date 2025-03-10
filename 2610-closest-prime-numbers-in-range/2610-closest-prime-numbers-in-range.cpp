4380da003ac563f80c6d767c0cfe40789eb1320c
Rohit Nain <115409470+rohit141914@users.noreply.github.com>
2025-03-10 15:25:46 +0530

    Time: 149 ms (82.44%), Space: 158.9 MB (14.99%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-    bool isPrime(int n){
-        for(int i=2;i*i<=n;i++){
-            if(n%i==0){
-                return 0;
-            }
-        }
-        return 1;
-    }
     vector<int> closestPrimes(int left, int right) {
-        vector<int>arr;
-        if(left==1) left++;
+        vector<int>v(right+1,1);
+        v[0]=0;
+        v[1]=0;
+        for(int i=2;i*i<=right;i++){
+            if(v[i]==1){
+                for(int j=i+i;j<=right;j=j+i){
+                    v[j]=0;
+                }
+            }
+        }
+        vector<int>ans{-1,-1};
+        int d=INT_MAX;
+        int p=-1;
         for(int i=left;i<=right;i++){
-            if(isPrime(i)){
-                arr.push_back(i);
+            if(v[i]==1){
+                if(p==-1){
+                    p=i;
+                }
+                else if(ans[0]==-1){
+                    ans[0]=p;
+                    ans[1]=i;
+                    p=i;
+                    d=ans[1]-ans[0];
+                }
+                else{
+                    if(d>i-p){
+                        ans[0]=p;
+                        ans[1]=i;
+                        d=i-p;
+                    }
+                        p=i;
+                }
             }
         }
-        if(arr.size()<2){
-            arr.clear();
-            arr.push_back(-1);
-            arr.push_back(-1);
-            return arr;
-        }
-        int d=arr[1]-arr[0];
-        int a=arr[0];
-        int b=arr[1];
-        for(int i=2;i<arr.size();i++){
-            if(arr[i]-arr[i-1]<d){
-                a=arr[i-1];
-                b=arr[i];
-                d=b-a;
-            }
-        }
-        arr.clear();
-        arr.push_back(a);
-        arr.push_back(b);
-        return arr;
+        return ans;
     }
 };
