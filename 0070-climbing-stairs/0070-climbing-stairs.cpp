9d5e28b9bb24e9e962352b0c6de3e6f5f327ac36
Rohit Nain <rohitn@callhub.io>
2026-01-03 18:10:50 +0530

    Time: 0 ms (100%), Space: 8.7 MB (16.69%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    unordered_map<int,int>temp;
+    int climbStairs(int n) {
+        if(n<0) return 0;
+        if(n==0) return 1;
+        // int ans= climbStairs(n-1)+climbStairs(n-2);
+        int a=0;
+        int b=0;
+        if(temp.find(n-1)!=temp.end()){
+            a=temp[n-1];
+        }
+        else{
+            a=climbStairs(n-1);
+            temp[n-1]=a;
+        }
+        if(temp.find(n-2)!=temp.end()){
+            b=temp[n-2];
+        }
+        else{
+            b=climbStairs(n-2);
+            temp[n-2]=b;
+        }
+        return a+b;
+    }
+};
